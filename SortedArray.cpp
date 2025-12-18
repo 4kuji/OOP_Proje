8fa7d8b47eb8341c19fefa5b08417627d2fa8289
Zenuba16 <85034216+Zenuba16@users.noreply.github.com>
2025-12-19 00:42:38 +0300

    SortedArray icin binary search algoritmasi eklendi

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -18,3 +18,22 @@ void SortedArray::push(int value) {
     (*this)[i + 1] = value;
 }
 
+// Binary Search Algoritmasi
+int SortedArray::binarySearch(int value) const {
+    int left = 0;
+    int right = getSize() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int midVal = (*this)[mid];
+
+        if (midVal == value)
+            return mid; // Bulundu
+
+        if (midVal < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1; // Bulunamadi
+}
